7cbe4f2003001bc2850e790c3c60f2503d25ab16
Kartik <kartiksachdeva3@gmail.com>
2020-12-12 14:46:43 +0530

    Nabar-extend

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './style.css';
 import React from "react";
 import { render } from "react-dom";
 import Nav from "./Components/Navbar/Navbar";
+import Sidebar from './Components/Sidebar/Sidebar';
 
 
 function App() {
   return (
       <div>
           <Nav />
+          <Sidebar />
       </div>
   );
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Navbar from 'react-bootstrap/Navbar';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 class Nav extends React.Component{
   render() {
     return (
-      <Navbar bg="dark">
-      <Navbar.Brand>
-        SplitBills
-      </Navbar.Brand>
-    </Navbar>);
+      <Navbar bg="dark" variant="dark" >
+      <Navbar.Brand>SplitBills</Navbar.Brand>
+      </Navbar>
+
+    );
   }
 }
 export default Nav;
